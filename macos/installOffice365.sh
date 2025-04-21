47d1949fe0a5db393635828ef7d5e897bf5ad2bf
inLoad <ceo.inload@gmail.com>
2025-04-21 22:04:14 +0200

    Update installOffice365.sh

--- macos/installOffice365.sh
+++ macos/installOffice365.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 currentProject="Microsoft Office 365"
-echo "✳️ Installation of $currentProject"
+echo "✳️ Installation of $currentProject\n"
 sleep 3
 brew install --cask microsoft-word microsoft-excel microsoft-powerpoint
-echo "✅ $currentProject Installation done !"
+echo "✅ $currentProject Installation done !\n"
 sleep 5	
