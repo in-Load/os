c1612536aec4bf9a557e3eec4f880455ab4b04c1
inLoad <ceo.inload@gmail.com>
2025-04-21 21:50:58 +0200

    Add script

---
+++ macos/installHomebrew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+currentProjectName="Homebrew"
+echo "✳️ Installation of $currentProjectName"
+sleep 3
+brew install --cask microsoft-word microsoft-excel microsoft-powerpoint
+echo "✅ Intallation of $currentProjectName done !"
+sleep 5	
